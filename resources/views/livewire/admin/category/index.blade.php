eb3568592dbd197bc2286b93cd4a75bd058e5849
eieikhaing <eek752000@gmail.com>
2023-01-08 11:28:49 +0630

    Finishing Edit and Delete

--- resources/views/livewire/admin/category/index.blade.php
+++ resources/views/livewire/admin/category/index.blade.php
@@ -1,3 +1,25 @@
+<div>
+<!-- Modal -->
+<div wire:ignore.self class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Category Delete</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form wire:submit.prevent="destroyCategory">
+        <div class="modal-body">
+            <h6>Are you sure you want to delete this data?</h6>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Yes. Delete</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
 <div class="row">
     <div class="col-md-12">
         @if (session('message'))
@@ -27,7 +49,7 @@
                             <td>{{ $category->status=='1' ?'Hidden':'Visible' }}</td>
                             <td>
                                 <a href="{{url('admin/category/'.$category->id.'/edit')  }}" class="btn btn-success">Edit</a>
-                                <a href="" class="btn btn-danger">Delete</a>
+                                <a href="#" wire:click="deleteCategory({{ $category->id }})" data-bs-toggle="modal" data-bs-target="#deleteModal" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
                         @endforeach
@@ -42,3 +64,11 @@
         </div>
     </div>
 </div>
+</div>
+@push('script')
+    <script>
+        window.addEventListener('close-modal',event=>{
+            $('#deleteModal').modal('hide');
+        })
+    </script>
+@endpush
