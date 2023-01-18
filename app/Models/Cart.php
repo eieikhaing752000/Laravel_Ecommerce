220faeb9ed6d8e5afa4fdbfb6205b661e3d17741
eieikhaing <eek752000@gmail.com>
2023-01-18 11:04:40 +0630

    Finishing Cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
@@ -12,4 +13,12 @@ class Cart extends Model
     protected $fillable=[
         'user_id','product_id','product_color_id','quantity'
     ];
+    public function product():BelongsTo
+    {
+        return $this->belongsTo(Product::class,'product_id','id');
+    }
+    public function productColor():BelongsTo
+    {
+        return $this->belongsTo(ProductColor::class,'product_color_id','id');
+    }
 }
