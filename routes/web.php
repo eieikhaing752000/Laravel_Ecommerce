b5cbd800608fcf97139f0278a342627330d6deae
eieikhaing <eek752000@gmail.com>
2023-02-03 21:10:25 +0630

    Updating User Profile

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,9 @@ Route::middleware(['auth'])->group(function(){
    Route::get('payment',[App\Http\Controllers\Frontend\CheckoutController::class,'payment']);
    Route::get('orders',[App\Http\Controllers\Frontend\OrderController::class,'index']);
    Route::get('orders/{orderId}',[App\Http\Controllers\Frontend\OrderController::class,'show']);
+
+   Route::get('profile',[App\Http\Controllers\Frontend\UserController::class,'index']);
+   Route::post('profile',[App\Http\Controllers\Frontend\UserController::class,'updateUserDetails']);
 });
 
 Route::get('thank-you',[App\Http\Controllers\Frontend\FrontendController::class,'thankyou']);
